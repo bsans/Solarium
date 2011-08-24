2dba11c88db74cf50b2d026807205234bf63596d
Bodhi Sansom <elijah.bodhi@gmail.com>
2011-08-24 08:14:11 -0700

    writing colormaps to files

--- testcolor.py
+++ testcolor.py
@@ -2,8 +2,10 @@
 import PIL
 from PIL import Image, ImageDraw
 import os
+import sys
 import math
 import pprint
+import pickle
 # im = Image.open("/Users/Bodhi/Desktop/solarium/whiteimage.jpg")
 #draw = ImageDraw.Draw(im)
 #draw.line((0, im.size[1], im.size[0], 0), fill=128)
@@ -11,14 +13,21 @@ import pprint
 BASENIGHT_COLOR=(3, 6, 15)
 BASEDAY_COLOR=(120, 181, 244)
 DUSK_COLOR=(180, 154, 127)
-#draw.line((0,0) + (200,100), BASEDAY_COLOR)
-#draw.arc((0,0,100,100), 0, 90, BASENIGHT_COLOR) 
-#image1.show()
+SKYBLUE2 = (1, 50, 109)
+SKYBLUE1 = (17, 106, 188)
+PREDAWN_HORIZON = (61, 57, 52)
+PREDAWN_HORIZ_2 = (23, 38, 54)
+PREDAWN_HORIZ_3 = (8, 16, 35)
+PREDAWN_ZENITH = (4, 7, 14)
+BLACK = (0, 0, 0)
+GRAYSCALE_20 = (20, 20, 20)
 
 def linear_gradient(start_color, end_color, num_steps):
   """
   defines color gradient to use, returning a list of length steps as the values
-  colors are taken as 3-tuples.  I.e. a list of 3-tuples is returned
+  colors are taken as 3-tuples.  I.e. a list of 3-tuples is returned.
+  casting to int is not done yet because the math will not work if so. It must 
+  be done later before drawing the image, or making final colormap.
   """
   num_steps = num_steps + 0.0 # making it a float
   color_step_R = (end_color[0] - start_color[0]) / num_steps 
@@ -26,18 +35,55 @@ def linear_gradient(start_color, end_color, num_steps):
   color_step_B = (end_color[2] - start_color[2]) / num_steps
   lin_grad = [start_color] # initialize list
   for i in range(int(num_steps)):
-    lin_grad.append((lin_grad[-1][0]+color_step_R, lin_grad[-1][1] + color_step_G,
-                    lin_grad[-1][2]+color_step_B))
+    val1 = lin_grad[-1][0] + color_step_R
+    val2 = lin_grad[-1][1] + color_step_G
+    val3 = lin_grad[-1][2] + color_step_B
+    # thresholding negative colors to be black
+    if val1 < 0:
+      val1 = 0
+    if val2 < 0:
+      val2 = 0
+    if val3 < 0:
+      val3 = 0
+    lin_grad.append((val1, val2, val3))
   return lin_grad
 
-#testout = linear_gradient((0,0,0), (100,100,100), 20)
-#pp = pprint.PrettyPrinter(indent=4)
-#pp.pprint(testout)
+
+def multi_step_gradient(colors, col_list_pos):
+  """
+  Takes a list of colors, with an equal length list of positions where those
+  colors are supposed to be positioned in the entire colormap.  Calculates
+  the values for the entire colormap using linear gradient steps between each
+  pair of color values.
+  """
+
+  # assume first color is at col_list_pos equal to pixel 1
+  # also assume last color is at col_list_pos equal to last pixel (181)
+  colormap = []
+  for idx in range(len(colors) - 1):
+    steps = col_list_pos[idx + 1] - col_list_pos[idx]
+    colormap = colormap + linear_gradient(colors[idx], colors[idx + 1], steps)
+  colormap = colormap[0:180]
+  return colormap
+
+def int_cast(cmap):
+  """
+  Necessary casting of all colormap values to int in order to make usable by
+  downstream consumers.
+  """
+  int_red = [int(x[0]) for x in cmap]
+  int_green = [int(x[1]) for x in cmap]
+  int_blue = [int(x[2]) for x in cmap]
+  output = []
+  for idx, elem in enumerate(int_red):
+    output.append((elem, int_green[idx], int_blue[idx]))
+  return output
 
 def draw_lines(num_lines, line_length, color_list, image):
-  """# lines have a length and a color
-# eventually "lines" will be a single pixel of a color
-  # for now, they have a start and end and color
+  """
+  lines have a length and a color
+  eventually "lines" will be a single pixel of a color
+  for now, they have a start and end and color
   color_list and line_length will set the small lines we add up
   """
   # let's say they're vertical for now, and one pixel wide
@@ -122,8 +168,7 @@ def add_sun(suncmap, othercmap):
   The sun colormap is intended to be RGB values (3-tuples) which *replace*
   the corresponding RGB values in the other colormap.  Not adding RGBs, but
   replacing the ones in the other colormap.  The suncmap is also assumed to
-  have (0, 0, 0) values outside of the sun, and these are disregarded.
-  """
+  have (0, 0, 0) values outside of the sun, and these are disregarded.  """
   cmapout = []
   lastindex = 0
   for idx, rgb in enumerate(suncmap):
@@ -140,9 +185,21 @@ def output_cmap(cmap):
   Need to spit out 3-tuple lists in a nice format for importing to C as an include
   """
   for idx, i in enumerate(cmap):
-    cmap[idx] = "/".join([str(x) for x in i])
+    cmap[idx] = '_'.join([str(x) for x in i]) # escaping forward slash
   output = ", ".join([str(x) for x in cmap])
   return output
+
+def output_to_file(cmap):
+  """
+  Uses output_cmap to write to command line argument specified file.
+  """
+  f = open(sys.argv[1], 'w')
+  #pickle.dump(output_cmap(cmap), f)
+  output = output_cmap(cmap)
+  output = r'%s' % output # makes it a "raw" string for writing to file
+  f.write(output)
+  f.close()
+
 
 image1 = Image.new("RGB", (181, 181), BASENIGHT_COLOR)
 image2 = Image.new("RGB", (181, 181), BASENIGHT_COLOR)
@@ -150,17 +207,19 @@ image3 = Image.new("RGB", (181, 181), BASENIGHT_COLOR)
 cmaptest1 = linear_gradient(BASEDAY_COLOR, DUSK_COLOR, 180)
 cmaptest2 = linear_gradient(BASEDAY_COLOR, BASENIGHT_COLOR, 180)
 cmaptest3 = linear_gradient(DUSK_COLOR, BASENIGHT_COLOR, 180)
+noon_cmap1 = linear_gradient(SKYBLUE1, SKYBLUE2, 180)
+#mid_morning_cmap1 = 
 testinterout = interpolate_colormaps(cmaptest2, 0, cmaptest1, 3, 2)
 pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(testinterout)
+#pp.pprint(testinterout)
 #pp = pprint.PrettyPrinter(indent=4)
 #pp.pprint(newcolors)
-image1output = draw_lines(181, 181, cmaptest1, image3)
-image1output.show()
-image2output = draw_lines(181, 181, cmaptest2, image3)
-image2output.show()
-interoutput = draw_lines(181, 181, testinterout, image3)
-interoutput.show()
+#image1output = draw_lines(181, 181, cmaptest1, image3)
+#image1output.show()
+#image2output = draw_lines(181, 181, cmaptest2, image3)
+#image2output.show()
+#interoutput = draw_lines(181, 181, testinterout, image3)
+#interoutput.show()
 
 testaddsun = []
 for i in range(40):
@@ -175,6 +234,33 @@ for i in range(181):
   addsuntestbase.append(BASEDAY_COLOR)
 suntestcmap = add_sun(testaddsun, addsuntestbase)
 image4output = draw_lines(181, 181, suntestcmap, image4)
-image4output.show()
+#image4output.show()
 #csv_writer = lambda rows: "\n".join([", ".join([x for x in row]) for row in rows])
-print output_cmap(suntestcmap)
+#print output_cmap(suntestcmap)
+dawncmap = linear_gradient(PREDAWN_HORIZON, PREDAWN_ZENITH, 180)
+
+# testing multi_step_gradient
+mycolors = [PREDAWN_HORIZON, PREDAWN_HORIZ_2, PREDAWN_HORIZ_3, PREDAWN_ZENITH]
+mycolorpos = [0, 20, 50, 180]
+multicmap = multi_step_gradient(mycolors, mycolorpos)
+#pp.pprint(multicmap)
+image6 = Image.new("RGB", (181, 181), SKYBLUE1)
+#dawntest = draw_lines(181, 181, multicmap, image6)
+#dawntest.show()
+#print output_cmap(multicmap)
+#pp.pprint(dawncmap)
+#image7 = Image.new("RGB", (181, 181), SKYBLUE1)
+#control = draw_lines(181, 181, dawncmap, image6)
+#control.show()
+BLACK = (0, 0, 0)
+GRAYSCALE_20 = (20, 20, 20)
+gray20cmap = int_cast(linear_gradient(BLACK, GRAYSCALE_20, 180))
+image8 = Image.new("RGB", (181, 181), BLACK)
+#gray20 = draw_lines(181, 181, gray20cmap, image8)
+print output_cmap(gray20cmap)
+output_to_file(gray20cmap)
+print
+print len(gray20cmap)
+#gray20.show()
+
+
